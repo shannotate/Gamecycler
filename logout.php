25091a5c8a5b8ac11b901b3466cb9e503f237ecc
Shane Vetti <ShaneVetti@gmail.com>
2012-02-27 22:24:21 -0500

    added search, changed all pages to .php

---
+++ logout.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GameCycler</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+<!--
+genesis, a free CSS web template by spyka Webmaster (www.spyka.net)
+
+Download: http://www.spyka.net/web-templates/genesis/
+
+License: Creative Commons Attribution
+//-->
+</head>
+<body>
+<div id="container">
+	<div id="header">
+    	<h1><a href="/">GameCycler</a></h1>
+        <h2>Where dead games are reborn</h2>
+        <div class="clear"></div>
+    </div>
+    <div id="nav">
+    	<ul>
+        	<li class="start"><a href="index.php">Home</a></li>
+            <li><a href="createaccount.php">Sign Up</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="gamesubmit.php">Submit a Game</a></li>
+            <li><a href="search.php">Search Games</a></li>
+            <li><a href="usersearch.php">Search Users</a></li>
+        </ul>
+    </div>
+    <div id="body">
+	
+		<div id="content">
+			<?php
+			session_start();
+			session_unset();
+			?>
+			<h2>You have logged out!</h2>
+            <p><strong>GameCycler</strong> is a marketplace for buying, trading, and selling previously-owned video games on their original storage mediums.</p>
+        </div>
+        
+        <div class="sidebar">
+            <ul>	
+               <li>
+                    <h3>Navigate</h3>
+                    <ul class="blocklist">
+                        <li><a href="index.php">Home</a></li>
+						<li><a href="createaccount.php">Create an Account!</a></li>
+                        <li><a href="gamesubmit.php">Post a Game!</a></li>
+						<li><a href="search.php">Search for Games!</a></li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <h3>About</h3>
+                    <ul>
+                        <li>
+                        	<p style="margin: 0;">GameCycler was created by four students at the University of Mary Washington.  It it copyrighted under the Creative Commons license</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul> 
+        </div>
+    	<div class="clear"></div>
+    </div>
+    <div id="footer">
+        <div class="footer-content">
+            <div class="clear"></div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; GameCycler 2012 | Design by GameCycler Team | Template Source: <a href="http://www.spyka.net" title="spyka webmaster">spyka Webmaster</a> <a href="http://www.justfreetemplates.com">Free Web Templates</a></p>
+         </div>
+    </div>
+</div>
+</body>
+</html>
